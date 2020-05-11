88be656dc195769cfd5b8b0d0e177f2ff8dc3fea
akshat2013soni <56476130+akshat2013soni@users.noreply.github.com>
2020-05-11 16:35:11 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include "headers.h"
 
 int main(void) {
-  srand(time(0));
+srand(time(0));	
+int results[10],sum=0;
+float avg,stddev=0.0;
+int temp = doExperiments(2000, 3, 0);
 
-  int temp = doExperiments(2000, 3, 0);
-
-  printf("%d", temp);
-
-  for (int i = 0; i < 10; i++) {
-    /* code */
-  }
-
+printf("%d", temp);
+for (int i = 0; i < 10; i++){
+    results[i]=doExperiments(2000,3,0);
+  	sum+=results[i];
+    }
+avg=(float)sum/(10.0);
+for(int i=0;i<10;i++)
+    {
+    stddev+=pow((float)results[i]-avg,2);
+    }
+stddev/=10.0;
+stddev=sqrt(stddev);
   return 0;
-}
